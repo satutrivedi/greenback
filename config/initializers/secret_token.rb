352c07bfa829841977b6b4eed8615b014c6838cb
Satu Trivedi <trivedi.satu@gmail.com>
2014-05-10 02:51:14 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yumster::Application.config.secret_key_base = '061a901fc18faa267f109ac3c7d8904b440d7cfccc634faba87f426917763fea7cd5b08667811482bd1c8444b2c1a3962640fae6a3e79a6d93d59408eca28b15'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Yumster::Application.config.session_store :cookie_store, key: '_yumster_session'
